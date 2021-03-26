3aa47981a2285575ae789639ec88b5355ad3f88e
Bruno <brunomendesccb@gmail.com>
2021-03-26 13:00:14 -0300

    Added comments and email sharing

--- site_bruno/settings.py
+++ site_bruno/settings.py
@@ -121,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# EMAIL_HOST = 'smtp.gmail.com'
+
+# EMAIL_HOST_USER = 'type_email'
+
+# EMAIL_HOST_PASSWORD = 'type_password'
+
+# EMAIL_PORT = 587
+
+# EMAIL_USE_TLS = True
